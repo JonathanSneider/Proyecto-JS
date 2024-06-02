ad836828dff19bd3ff6495a834cf2c359cd34da9
Ospina115 <158192117+Ospina115@users.noreply.github.com>
2024-06-01 20:11:36 -0500

    preparacion ultimo web component
    ya cree los archivos, hice las respectivas importaciones y edité unas cosas de la interfaz grafica, falta agregar todo lo loquito

---
+++ ProyectoJS/src/components/InformesDetallados.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '/InformesDetallados.css';
+
+//1) CALCULAR LA EFICIENCIA OPERATIVA
+//se necesita Productividad, Costos Operativos, Tiempo de Ciclo, Tasa de Defectos
+
+//2) CALCULAR EL COSTO DE MANO DE OBRA
+//se necesita Salario Base, Horas Trabajadas, Beneficios y Prestaciones, Costos Indirectos
