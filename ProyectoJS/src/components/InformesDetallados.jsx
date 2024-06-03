35546ccfa157b2570df3fc13b54f3a399ee979f7
Ospina115 <158192117+Ospina115@users.noreply.github.com>
2024-06-03 12:35:19 -0500

    Funciones de Informes detallados
    ya estan todas las funciones para crear todos los informes, falta conectar los datos y graficarlos para que el usuario los vea

--- ProyectoJS/src/components/InformesDetallados.jsx
+++ ProyectoJS/src/components/InformesDetallados.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 import '/InformesDetallados.css';
 
-//1) CALCULAR LA EFICIENCIA OPERATIVA
-//se necesita Productividad, Costos Operativos, Tiempo de Ciclo, Tasa de Defectos
 
-//2) CALCULAR EL COSTO DE MANO DE OBRA
-//se necesita Salario Base, Horas Trabajadas, Beneficios y Prestaciones, Costos Indirectos
+//FUNCIONES PARA SACAR LA EFICIENCIA OPERATIVA
+
+function Productividad(ProductosTerminados, HorasProduccion){
+    return ProductosTerminados/HorasProduccion
+} //CALCULAR LA PRODUCTIVIDAD
+
+function CostosOperativosUnidad(CostosOperativos, ProductosTerminados){
+    return CostosOperativos/ProductosTerminados
+} // CALCULAR LOS COSTOS OPERATIVOS POR UNIDAD
+
+function TasaDefectos(ProductosDefectuosos, ProductosFabricados){
+    return (ProductosDefectuosos/ProductosFabricados)*100
+} //CALCULAR LA TASA DE DEFECTOS
+
+function ProduccionEfectiva(ProductosTerminados, ProductosDefectuosos){
+    return ProductosTerminados-ProductosDefectuosos
+} //CALCULAR LA PRODUCCION EFECTIVA
+
+function EficienciaOperativa(ProduccionEfectiva, CostosOperativos){
+    return ProduccionEfectiva/CostosOperativos
+}
+
+
+
+
+function SalarioBaseTotal(SalarioBaseHora, HorasTrabajadas){
+    return SalarioBaseHora*HorasTrabajadas //HAY QUE SUMAR TODOS LOS SALARIOS BASES DE TODOS LOS EMPLEADOS
+}
+
+function BeneficiosPrestaciones(BeneficiosPrestacionesEmpleado){
+    return BeneficiosPrestacionesEmpleado  //HAY QUE SUMAR TODOS LOS BENEFICIOS Y PRESTACIONES DE TODOS LOS EMPLEADOS
+}
+
+function CostosIndirectos(CostosIndirectosEmpleado){
+    return CostosIndirectosEmpleado //HAY QUE SUMAR TODOS LOS COSTOS INDIRECTOS DE TODOS LOS EMPLEADOS
+}
+
+function CostoDeManoDeObra(SalarioBaseHora, HorasTrabajadas, BeneficiosPrestaciones){
+    return (SalarioBaseHora*HorasTrabajadas) + BeneficiosPrestaciones
+}
